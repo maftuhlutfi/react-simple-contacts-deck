008ab3e0ef55027f90977e6eef3cdb086861b5fa
maftuhlutfi <playmakernyomlo@gmail.com>
2020-07-06 15:29:01 +0700

    Updates

---
+++ precache-manifest.1c1ca9dc32dc19be62aad4055cfd2dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f8ae7f5e59ef17d92d2e2a9dcfb233",
+    "url": "/react-simple-contacts-deck/index.html"
+  },
+  {
+    "revision": "502946a4d3d329a6ea3f",
+    "url": "/react-simple-contacts-deck/static/css/main.a2174928.chunk.css"
+  },
+  {
+    "revision": "52342ffe419bd2355ce0",
+    "url": "/react-simple-contacts-deck/static/js/2.4afea482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-contacts-deck/static/js/2.4afea482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502946a4d3d329a6ea3f",
+    "url": "/react-simple-contacts-deck/static/js/main.1d6f86df.chunk.js"
+  },
+  {
+    "revision": "081a48c759e8ec081c8c",
+    "url": "/react-simple-contacts-deck/static/js/runtime-main.82e34b8e.js"
+  }
+]);
